6607fe89f305c1b2c34acd3062917e91cc511153
Aditi Kattuparambil <akattuparambil960@student.fuhsd.org>
2024-02-12 19:02:03 -0800

    added yearly

--- src/sections/YearlyCalendar/Planner.js
+++ src/sections/YearlyCalendar/Planner.js
@@ -1,45 +1,64 @@
 import ProductList from '../../components/ProductList';
-
+import Berry from "./images/Berry.png";
+import Neutral from "./images/Neutral.png";
+import Purple from "./images/Purple.png";
+import Green from "./images/Green.png";
+import Blue from "./images/Blue.png";
+import BerryCanva from "./pdfs/Berry Yearly Calendar.pdf";
+import NeutralCanva from "./pdfs/Neutral Yearly Calendar.pdf";
+import PurpleCanva from "./pdfs/Purple Yearly Calendar.pdf";
+import GreenCanva from "./pdfs/Green Yearly Calendar.pdf";
+import BlueCanva from "./pdfs/Blue Yearly Calendar.pdf";
 
 const planners = [
   {
     id: 1,
-    name: 'Product 1',
+    name: 'Berry Yearly Calendar',
     description: 'This is the description for Product 1.',
-    image: 'logo.svg',
+    image: Berry,
+    canva: "youtube.com",
+    download: BerryCanva,
   },
   {
     id: 2,
-    name: 'Product 1',
+    name: 'Neutral Yearly Calendar',
     description: 'This is the description for Product 1.',
-    image: 'logo.svg',
+    image: Neutral,
+    canva: "youtube.com",
+    download: NeutralCanva,
   },
   {
     id: 3,
-    name: 'Product 1',
+    name: 'Purple Yearly Calendar',
     description: 'This is the description for Product 1.',
-    image: 'logo.svg',
+    image: Purple,
+    canva: "youtube.com",
+    download: PurpleCanva,
   },
   {
     id: 4,
-    name: 'Product 1',
+    name: 'Green Yearly Calendar',
     description: 'This is the description for Product 1.',
-    image: 'logo.svg',
+    image: Green,
+    canva: "youtube.com",
+    download: GreenCanva,
   },
   {
     id: 5,
-    name: 'Product 1',
+    name: 'Blue Yearly Calendar',
     description: 'This is the description for Product 1.',
-    image: 'logo.svg',
+    image: Blue,
+    canva: "youtube.com",
+    download: BlueCanva,
   }
 ];
 
-  function MonthlyCalendars(){
+  function YearlyCalendar(){
     return(
-      <section id = "monthly-calendars">
-        <h2>Monthly Calendars</h2>
+      <section id = "yearly-calendar">
+        <h2>Yearly Calendars</h2>
         <ProductList products={planners} />
       </section>
     )
   }
-  export default MonthlyCalendars;
+  export default WeeklyStudyPlanner;
